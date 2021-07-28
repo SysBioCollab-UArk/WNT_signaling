58108e26bb6ea8e644c3ce047392bd2175766f14
ashleyfer01 <ajf012@uark.edu>
2021-07-28 17:03:36 -0500

    destruction complex code

--- destcpx.py
+++ destcpx.py
@@ -7,8 +7,8 @@ Model()
 
 #monomer
 Monomer('Axin', ['Bcat', 'gsk3', 'ck1a', 'apc'])
-Monomer('GSK3', ['axin', 'apc'])
-Monomer('CK1A', ['axin', 'apc'])
+Monomer('GSK3', ['axin', 'apc','bcat'])
+Monomer('CK1A', ['axin', 'apc', 'bcat')
 Monomer('APC', ['axin', 'bcat', 'ck1a', 'gsk3'], {'ck1a':['p', 'np'], 'gsk3': ['p', 'np']})#ASK IF THE STATE OF THESE MONOMERS ARE NECESSARY
 Monomer('Bcat', ['axin', 'apc', 'ck1a', 'gsk3', 'btrcp'], {'ck1a':['p', 'np'], 'gsk3': ['p', 'np'], 'apc': ['aa15', 'aa20'], 'btrcp': ['x', 'ub']})# CK1A site can be phosphorylated or not phosphorylated as well as GSK3 site
 Monomer('BTRCP', ['bcat'])
@@ -22,10 +22,10 @@ Parameter('Bcat_0', 100)
 Parameter('BTRCP', 50)
 
 Initial(Axin(bcat=None, gsk3=None, ck1a=None, apc=None), Axin_0)
-Initial(GSK3(axin=None, apc=None), GSK3_0)
-Initial(CK1A(axin=None, apc=None), CK1A_0)
-Initial(APC(axin=None, bcat=1, ck1a='np', gsk3='np') % Bcat(apc=1), APC_0)
-Initial(Bcat(axin=None, apc='aa15', ck1a='np', gsk3='np', btrcp='x'), Bcat_0)
+Initial(GSK3(axin=None, apc=None,bcat=None), GSK3_0)
+Initial(CK1A(axin=None, apc=None, bcat=None), CK1A_0)
+Initial(APC(axin=None, bcat=None, ck1a='np', gsk3='np') % Bcat(apc=1), APC_0)
+Initial(Bcat(axin=None, apc=None, ck1a='np', gsk3='np', btrcp='x'), Bcat_0)
 Initial(BTRCP(bcat=None))
 
 #observables
@@ -42,11 +42,16 @@ Observable('BTRCP_tot', BTRCP())
 
 #Rate constants
 Parameter('k_gsk3_ckia_axin', 100)# rate at which Axin binds to gsk3 and ck1a
-
+Parameter('Bcat_join_the_complex', 150)
 
 #Rules
 
 #Axin binds to gsk3 and ck1a
-Rule('Axin_bindind', Axin(gsk3b=None,ck1a=None) % GSK3B(axin=None) % CK1A(axin=none) >> \
-     Axin(gsk3b=3, ck1a=6) % GSK3(axin=3) % CK1A(axin=6), k_gsk3_ckia_axin)# not sure if it can be reversible
-Rule('Bcat_Dcomplex',Bcat(axin= None, apc='aa15') % Axin(bcat=None, gsk3b=3, ck1a=6) )
+Rule('Axin_bindind', Axin(gsk3b=None,ck1a=None, apc=None) % GSK3B(axin=None) % CK1A(axin=none) % APC(axin=None) >> \
+     Axin(gsk3b=3, ck1a=6,apc=12) % GSK3(axin=3) % CK1A(axin=6),APC(axin=12), k_gsk3_ckia_axin)# not sure if it can be reversible
+
+#Bcat entering the destruction complex
+Rule('Bcat_Dcomplex',Bcat(axin= None, apc=None) % Axin(bcat=None, gsk3b=3, ck1a=6) % APC(bcat=None,axin=12) >> \
+     Bcat(axin=7,apc='15aa') % Axin(bcat=7, gsk3b=3, ck1a=6) % APC(bcat=7, axin=12), Bcat_join_the_complex )# still confused about the site at where APC(binds Bcat)
+#GK1A phosphorilates Bcat
+Rule('Bcat_phos_ck1a', Bcat(ck1a='np',axin='4',apc='15aa') % CK1A(axin=9,bcat=None) )
