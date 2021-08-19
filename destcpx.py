742aca81560b92edf510611352519782b5f75869
ashleyfer01 <ajf012@uark.edu>
2021-08-18 20:58:30 -0500

    adding one comment to the new version for future discussion

--- destcpx.py
+++ destcpx.py
@@ -41,20 +41,28 @@ Observable('Bcat_ub', Bcat(state='ub'))
 Observable('Btrcp_tot', Btrcp())
 
 # Rate constants
-
-# Rules
-
-# Axin binding rules
 Parameter('kf_axin_ck1a',1)
 Parameter('kf_axin_gsk3',1)
 Parameter('kf_axin_apc',1)
+Parameter('kf_bcat_dtcpx', 1)
+Parameter('kf_bcat_apc', 1)
+Parameter('kf_bcat_phos_gsk3', 1)
+Parameter('kf_bcat_phos_ck1a', 1)
+Parameter('kf_phos_ck1a', 2)
+Parameter('kf_bcat_binds_apc', 1)
+Parameter('k_btrcp_binds_bcat', 1)
+Parameter('k_bcat_ubiq', 1)
+Parameter('k_bcat_release', 2)
+# Rules
+
+# Axin binding rules
+
 Rule('axin_binds_ck1a', Axin(ck1a=None) + Ck1a(axin=None) >> Axin(ck1a=1) % Ck1a(axin=1), kf_axin_ck1a)
 Rule('axin_binds_gsk3', Axin(gsk3=None) + Gsk3(axin=None) >> Axin(gsk3=1) % Gsk3(axin=1), kf_axin_gsk3)
 Rule('axin_binds_apc', Axin(apc=None) + Apc(axin=None) >> Axin(apc=1) % Apc(axin=1), kf_axin_apc)
 
 # Bcat into dest comp
-Parameter('kf_bcat_dtcpx', 1)
-Parameter('kf_bcat_apc', 1)
+
 Rule('Bcat_binds_dtcpx', Bcat(top=None) + Axin(bcat=None, ck1a=ANY, gsk3=ANY, apc=ANY) >> \
      Bcat(top=1) % Axin(bcat=1, ck1a=ANY, gsk3=ANY, apc=ANY), kf_bcat_dtcpx )
 Rule('Bcat_binds_aa15', Bcat(top=ANY,bottom=None) + Apc(aa15=None) >> Bcat(top=ANY, bottom=1) % Apc(aa15=1), kf_bcat_apc)
@@ -63,18 +71,17 @@ Rule('Bcat_binds_aa15', Bcat(top=ANY,bottom=None) + Apc(aa15=None) >> Bcat(top=A
 
 
 # Bcat phosphorilated by gsk3 and ck1a
-Parameter('kf_bcat_phos_gsk3', 1)
-Parameter('kf_bcat_phos_ck1a', 1)
+
 Rule('bcat_p_ck1a', Bcat(nterm='u', top=ANY) >> Bcat(nterm='p1', top=ANY), kf_bcat_phos_ck1a) # ck1a phos first
 Rule('bcat_p_gsk3', Bcat(nterm='p1', top=ANY) >> Bcat(nterm='p2', top=ANY), kf_bcat_phos_gsk3) # then gsk3b phos after
 
 # APC phosphorilated by ck1a
 #should this be reversible or onedirectional?---AF
-Parameter('kf_phos_ck1a', 2)
+
 Rule('apc_p_ck1a', Apc(state='u', axin=ANY) >> Apc(state='p', axin=ANY), kf_phos_ck1a)
 
 # phosphoralation forces bcat detach from axin
-Parameter('kf_bcat_binds_apc', 1)
+
 Rule('bcat_binds_apc', Bcat(top=1, nterm='p2') % Axin(bcat=1, ck1a=ANY, gsk3=ANY, apc=2) % \
      Apc(aa20=None, axin=2, state='p') >> Bcat(top=1, nterm='p2') % Axin(bcat=None, ck1a=ANY, gsk3=ANY, apc=2) \
      % Apc(aa20=1, axin=2, state='p'), kf_bcat_binds_apc)
@@ -83,7 +90,7 @@ Rule('bcat_binds_apc', Bcat(top=1, nterm='p2') % Axin(bcat=1, ck1a=ANY, gsk3=ANY
 
 
 #  Btrcp binds bcat
-Parameter('k_btrcp_binds_bcat', 1)
+
 Rule('btrcp_binds_bcat', Bcat(top=1, nterm='p2', btrcp=None) % Apc(state='p', aa20=1, axin=ANY) + Btrcp(bcat=None) >> \
      Bcat(top=1, nterm='p2', btrcp=2) % Apc(state='p', aa20=1, axin=ANY) % Btrcp(bcat=2), k_btrcp_binds_bcat)
 
@@ -91,11 +98,16 @@ Rule('btrcp_binds_bcat', Bcat(top=1, nterm='p2', btrcp=None) % Apc(state='p', aa
 
 
 #  Btrcp ubiquitinates bcat
-Parameter('k_bcat_ubiq', 1)
+
 Rule('Bcat_ubiq', Bcat(btrcp=1, state='x', top=ANY) % Btrcp(bcat=1) >> \
      Bcat(btrcp=1, state='ub', top=ANY) % Btrcp(bcat=1), k_bcat_ubiq)
 
 # Apc release bcat by dephos
-Parameter('k_bcat_release', 2)
+
 Rule('apc_release_bcat', Apc(state='p', aa20=1, axin=ANY) % Bcat(top=1, state='ub', btrcp=ANY) >> \
      Apc(state='u', aa20=None, axin=ANY) + Bcat(top=None, state='ub', btrcp=ANY), k_bcat_release)
+
+#running simulations
+tspan = np.linspace(0, 1, 101)
+sim = ScipyOdeSimulator(model, tspan, verbose=True)
+
