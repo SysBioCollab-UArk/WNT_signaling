f96210020abe8fbc3bac68a6efaf71b518832e83
ajmedina27 <ajmedina@uark.edu>
2021-11-10 18:37:26 -0600

    Updated rules

--- destcpx.py
+++ destcpx.py
@@ -76,21 +76,27 @@ Parameter('k_bcat_release', 2)
 
 # Axin binding rules
 
-Rule('axin_binds_ck1a', Axin(ck1a=None) + Ck1a(axin=None) | Axin(ck1a=1) % Ck1a(axin=1), kf_axin_ck1a, kr_axin_ck1a)
-Rule('axin_binds_gsk3', Axin(gsk3=None) + Gsk3(axin=None) | Axin(gsk3=1) % Gsk3(axin=1), kf_axin_gsk3, kr_axin_gsk3)
-Rule('axin_binds_apc', Axin(apc=None) + Apc(axin=None) | Axin(apc=1) % Apc(axin=1), kf_axin_apc, kr_axin_apc)
+Rule('axin_binds_ck1a', Axin(bcat=None,ck1a=None) + Ck1a(axin=None) | Axin(bcat=None,ck1a=1) % Ck1a(axin=1),
+     kf_axin_ck1a, kr_axin_ck1a)
+Rule('axin_binds_gsk3', Axin(bcat=None,gsk3=None) + Gsk3(axin=None) | Axin(bcat=None,gsk3=1) % Gsk3(axin=1),
+     kf_axin_gsk3, kr_axin_gsk3)
+Rule('axin_binds_apc', Axin(bcat=None,apc=None) + Apc(axin=None) | Axin(bcat=None,apc=1) % Apc(axin=1),
+     kf_axin_apc, kr_axin_apc)
+
 
 # Bcat into dest comp
 
-Rule('Bcat_binds_dtcpx', Bcat(top=None,bottom=None) + Axin(bcat=None, ck1a=ANY, gsk3=ANY, apc=ANY) | \
-     Bcat(top=1, bottom=None) % Axin(bcat=1, ck1a=ANY, gsk3=ANY, apc=ANY),  kf_bcat_dtcpx, kr_bcat_dtcpx)
+Rule('Bcat_binds_dtcpx', Bcat(top=None,bottom=None,nterm='u') + Axin(bcat=None, ck1a=ANY, gsk3=ANY, apc=ANY) % Apc(aa20=None) | \
+     Bcat(top=1, bottom=None,nterm='u') % Axin(bcat=1, ck1a=ANY, gsk3=ANY, apc=ANY) % Apc(aa20=None),   kf_bcat_dtcpx, kr_bcat_dtcpx)
 
-Rule('Bcat_binds_aa15', Bcat(top=ANY, bottom=None) % Apc(aa15=None) | Bcat(top=ANY, bottom=1) % Apc(aa15=1), kf_bcat_apc, kr_bcat_apc)
+
 Rule('bcat_p_ck1a', Bcat(top=ANY, nterm='u') >> Bcat(top=ANY, nterm='p1'), kf_bcat_phos_ck1a) # ck1a phos first
 Rule('bcat_unp1', Bcat(nterm='p1') >> Bcat(nterm='u'), k_dephos)
 Rule('bcat_p_gsk3', Bcat(top=ANY, nterm='p1') >> Bcat(top=ANY, nterm='p2'), kf_bcat_phos_gsk3) # then gsk3b phos after
 Rule('bcat_unp2', Bcat(nterm='p2') >> Bcat(nterm='p1'), k_dephos)
 
+Rule('Bcat_binds_aa15', Axin(bcat=1) % Bcat(top=1, bottom=None) % Apc(state='u',aa15=None) |
+     Axin(bcat=1) % Bcat(top=1, bottom=2) % Apc(state='u',aa15=2),kf_bcat_apc, kr_bcat_apc)
 
 # is apc % bcat at aa15 necessary? not sure when it unbinds but has to unbind
 # It aslso binds to the aa20 but when it is phosphorilated .According to the paper
@@ -104,11 +110,12 @@ Rule('bcat_unp2', Bcat(nterm='p2') >> Bcat(nterm='p1'), k_dephos)
 # APC phosphorilated by ck1a
 # should this be reversible or onedirectional?---AF
 
-Rule('apc_p_ck1a', Apc(state='u', axin=ANY) % Ck1a() % Gsk3() % Bcat(nterm='p2') >> Apc(state='p1', axin=ANY) % Ck1a() % \
-     Gsk3() % Bcat(nterm='p2'), kf_apc_phos_ck1a)
+Rule('apc_p_ck1a', Apc(aa15=1,state='u', axin=ANY) % Ck1a() % Gsk3() % Bcat(bottom=1,nterm='p2') >>
+     Apc(aa15=1,state='p1', axin=ANY) % Ck1a() % Gsk3() % Bcat(bottom=1,nterm='p2'), kf_apc_phos_ck1a)
 Rule('apc_unp1', Apc(state='p1') >> Apc(state='u'), k_dephos)
-Rule('apc_p_gsk3', Apc(state='p1', axin=ANY) % Ck1a() % Gsk3() % Bcat(nterm='p2') >> Apc(state='p2', axin=ANY) % Ck1a() % \
-     Gsk3() % Bcat(nterm='p2'), kf_apc_phos_gsk3)
+
+Rule('apc_p_gsk3', Apc(aa15=1,state='p1', axin=ANY) % Ck1a() % Gsk3() % Bcat(bottom=1,nterm='p2') >>
+     Apc(aa15=1,state='p2', axin=ANY) % Ck1a() % Gsk3() % Bcat(bottom=1,nterm='p2'), kf_apc_phos_gsk3)
 Rule('apc_unp2', Apc(state='p2') >> Apc(state='p1'), k_dephos)
 
 # phosphoralation forces bcat detach from axin
@@ -151,6 +158,9 @@ plt.legend(loc=0)
 plt.xlabel('time')
 plt.ylabel('concentration')
 
+print(len(model.species))
+for sp in model.species:
+     print(sp)
 plt.show()
 
 # print(model.monomers)
