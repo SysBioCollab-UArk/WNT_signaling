7f0149b6610e3ba87bee785f5d89a826601ffdd4
lh64 <lh64@cornell.edu>
2021-12-15 18:23:04 -0600

    Ashley and I started going through the rules carefully and confirming them. Got about half way done

--- destcpx.py
+++ destcpx.py
@@ -93,12 +93,12 @@ Parameter('k_btrcp_binds_bcat', 1)
 Parameter('k_bcat_ubiq', 1)
 Parameter('k_bcat_release', 0.1)
 Parameter('k_bcat_deg', 1)
-Parameter('k_apc_dephos', 10)
 
 # Rules
 
 # Axin binding rules
-
+# We require beta-catenin to NOT be bound for these binding event to occur
+# We assume beta-catenin can only bind when all three of Ck1a, Gsk3, and Apc are bound
 Rule('axin_binds_ck1a', Axin(bcat=None, ck1a=None) + Ck1a(axin=None) | Axin(bcat=None, ck1a=1) % Ck1a(axin=1),
      kf_axin_ck1a, kr_axin_ck1a)
 Rule('axin_binds_gsk3', Axin(bcat=None, gsk3=None) + Gsk3(axin=None) | Axin(bcat=None, gsk3=1) % Gsk3(axin=1),
@@ -107,43 +107,41 @@ Rule('axin_binds_apc', Axin(bcat=None, apc=None) + Apc(axin=None) | Axin(bcat=No
      kf_axin_apc, kr_axin_apc)
 
 # Bcat into dest comp
-
+# Here, we are requiring that the aa20 site of APC is NOT bound to another beta-catenin molecule in order for
+# beta-catenin to bind to Axin
 Rule('Bcat_binds_dtcpx',
-     Bcat(top=None, bottom=None, nterm='u') +
-     Axin(bcat=None, ck1a=ANY, gsk3=ANY, apc=ANY) % Apc(aa20=None) |
-     Bcat(top=1, bottom=None, nterm='u') % Axin(bcat=1, ck1a=ANY, gsk3=ANY, apc=ANY) % Apc(aa20=None),
+     Bcat(top=None, bottom=None, nterm='u') + Axin(bcat=None, ck1a=ANY, gsk3=ANY, apc=1) % Apc(axin=1, aa20=None) |
+     Bcat(top=2, bottom=None, nterm='u') % Axin(bcat=2, ck1a=ANY, gsk3=ANY, apc=1) % Apc(axin=1, aa20=None),
      kf_bcat_dtcpx, kr_bcat_dtcpx)
 
-# We think Bcat can only be phosphorylated and dephosphorylated when bound to Axin, not to APC
+# We think Bcat can be phosphorylated and dephosphorylated when bound to Axin, whether it's bound to APC or not
+
+# NOTE: Bcat can only be bound to Axin at the 'top' site if ck1a, gsk3, and apc are all also bound
+# Therefore, we don't need to explicitly include those three sites in the rule
+# Also, dephosphorylation (reverse part of the rule) is implicitly modeled as due to PP2A
 Rule('bcat_p_ck1a', Bcat(top=1, nterm='u') % Axin(bcat=1) | Bcat(top=1, nterm='p1') % Axin(bcat=1),
      kf_bcat_phos_ck1a, k_dephos)  # ck1a phos first
-# Rule('bcat_unp1',   Bcat(nterm='p1') >> Bcat(nterm='u'), k_dephos)
+
 Rule('bcat_p_gsk3', Bcat(top=1, nterm='p1') % Axin(bcat=1) | Bcat(top=1, nterm='p2') % Axin(bcat=1),
      kf_bcat_phos_gsk3, k_dephos)  # then gsk3b phos after
-# Rule('bcat_unp2',   Bcat(nterm='p2') >> Bcat(nterm='p1'), k_dephos)
 
-Rule('Bcat_binds_aa15', Axin(bcat=1) % Bcat(top=1, bottom=None) % Apc(state='u', aa15=None) |
-     Axin(bcat=1) % Bcat(top=1, bottom=2) % Apc(state='u', aa15=2), kf_bcat_apc, kr_bcat_apc)
+# Bcat binds to aa15 site of APC (it is assumed phosphorylation state of Bcat does not affect binding)
+Rule('Bcat_binds_aa15', Bcat(top=1, bottom=None) % Axin(bcat=1, apc=2) % Apc(axin=2, state='u', aa15=None) |
+     Bcat(top=1, bottom=3) % Axin(bcat=1, apc=2) % Apc(axin=2, state='u', aa15=3), kf_bcat_apc, kr_bcat_apc)
 
-# is apc % bcat at aa15 necessary? not sure when it unbinds but has to unbind
-# It aslso binds to the aa20 but when it is phosphorilated .According to the paper
-# As discussed below, we have suggested that the reason APC contains two different types of b-catenin binding motifs is because they may function differently within the destruction
-# complex.
+# APC phosphorylated by ck1a and gsk3
+# Dephosphorylation is assumed to be due to PP2A (implicit)
+Rule('apc_p_ck1a', Bcat(bottom=1, nterm='p2') % Apc(aa15=1, state='u', axin=ANY) % Ck1a() % Gsk3() >>
+     Bcat(bottom=1, nterm='p2') % Apc(aa15=1, state='p1', axin=ANY) % Ck1a() % Gsk3(), kf_apc_phos_ck1a)
 
-# Bcat phosphorylated by gsk3 and ck1a
-
-# According to a paper in the absence of WNT signals the destruction is activated and a balanced between unphos and phos B-catenin
-
-# APC phosphorylated by ck1a
-# should this be reversible or onedirectional?---AF
-
-Rule('apc_p_ck1a', Apc(aa15=1, state='u', axin=ANY) % Ck1a() % Gsk3() % Bcat(bottom=1, nterm='p2') >>
-     Apc(aa15=1, state='p1', axin=ANY) % Ck1a() % Gsk3() % Bcat(bottom=1, nterm='p2'), kf_apc_phos_ck1a)
 Rule('apc_unp1', Apc(state='p1', aa20=None) >> Apc(state='u', aa20=None), k_dephos)
 
-Rule('apc_p_gsk3', Apc(aa15=1, state='p1', axin=ANY) % Ck1a() % Gsk3() % Bcat(bottom=1, nterm='p2') >>
-     Apc(aa15=1, state='p2', axin=ANY) % Ck1a() % Gsk3() % Bcat(bottom=1, nterm='p2'), kf_apc_phos_gsk3)
+Rule('apc_p_gsk3', Bcat(bottom=1, nterm='p2') % Apc(aa15=1, state='p1', axin=ANY) % Ck1a() % Gsk3() >>
+     Bcat(bottom=1, nterm='p2') % Apc(aa15=1, state='p2', axin=ANY) % Ck1a() % Gsk3(), kf_apc_phos_gsk3)
+
 Rule('apc_unp2', Apc(state='p2', aa20=None) >> Apc(state='p1', aa20=None), k_dephos)
+
+##### ASHLEY AND I HAVE CONFIRMED RULES UP TO THIS POINT. WILL DO THE REST NEXT TIME --LAH #####
 
 # phosphorylation forces bcat detach from axin
 
@@ -172,11 +170,12 @@ Rule('apc_release_bcat',
      Bcat(top=1, bottom=2, state='ub') % Apc(aa20=1) % Btrcp(bcat=2) >>
      Bcat(top=None, bottom=2, state='ub') % Btrcp(bcat=2) + Apc(aa20=None), k_bcat_release)
 
-# APC degraded by proteosome
+# Bcat degraded by proteosome
 Rule('bcat_degradation', Bcat(top=None, bottom=2, state='ub') % Btrcp(bcat=2) >> Btrcp(bcat=None), k_bcat_deg)
 
-# dephosphorylation of APC after Bcat release
-Rule('apc_dephos', Apc(state='p2', aa20=None) >> Apc(state='u', aa20=None), k_apc_dephos)
+# dephosphorylation of APC after Bcat release (implicitly assumed to be due to PP2A)
+Rule('apc_dephos_p2_p1', Apc(state='p2', aa20=None) >> Apc(state='p1', aa20=None), k_dephos)
+Rule('apc_dephos_p1_u', Apc(state='p1', aa20=None) >> Apc(state='u', aa20=None), k_dephos)
 
 #running simulations
 tspan = np.linspace(0, 1, 101)
